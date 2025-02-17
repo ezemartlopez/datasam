37b2697a22831d90c7b96e122cdc9085b5962906
ezemartlopez <zenitram.e14@gmail.com>
2025-02-17 17:47:42 -0300

    componente button reutilizable de la app

--- src/components/ButtonComponent.tsx
+++ src/components/ButtonComponent.tsx
@@ -1,9 +1,11 @@
+import Link from "next/link";
+
 // ButtonComponent.tsx
 function ButtonComponent({text, href}: {text: string, href?: string}) {
   return (
-    <a href={href? href: ''}>
+    <Link href={href? href: ""}>
       <button type="button" className="w-min h-[50px] bg-green-500 hover:bg-green-600 transition-colors duration-500 rounded-lg px-[20px] flex justify-center items-center text-nowrap text-white font-rubik">{text}</button>
-    </a>
+    </Link>
   );
 }
 export default ButtonComponent;
