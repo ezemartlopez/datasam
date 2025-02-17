50e43edddb202124e3d1aef698d5707c675016ea
ezemartlopez <zenitram.e14@gmail.com>
2025-02-17 17:48:10 -0300

    footer de base de la app

---
+++ src/components/Layout/Footer.tsx
@@ -0,0 +1,11 @@
+// Footer.tsx
+function Footer() {
+  return (
+    <footer className="w-full h-auto">
+      <div className="w-full h-[100px] bg-green-700 flex justify-center items-center">
+        <p className="font-rubik text-sm sm:text-lg font-light text-center text-white">©2025 - DATA SAM - Universidad Nacional de San Martín. Todos los derechos reservados.</p>
+      </div>
+    </footer>
+  );
+}
+export default Footer;
