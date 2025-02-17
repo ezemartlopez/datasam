2b456cb25f161961d8d6bf6e689a8bf3ff490694
ezemartlopez <zenitram.e14@gmail.com>
2025-02-17 17:48:31 -0300

    fuente de la pagina web

---
+++ src/ui/Fonts.ts
@@ -0,0 +1,6 @@
+import { Rubik } from "next/font/google";
+
+export const rubikFont = Rubik({
+  variable: "--font-rubik", // Asegúrate de crear una variable para la fuente Rubik
+  subsets: ["latin"],
+});
