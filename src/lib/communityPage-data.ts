5ea5b254effb857f61862b6c56b683aea45e046e
ezemartlopez <zenitram.e14@gmail.com>
2025-02-18 23:01:01 -0300

    datos de la pagina d ela comunidad

---
+++ src/lib/communityPage-data.ts
@@ -0,0 +1,90 @@
+export interface LinkPage{
+  type: "whatsapp" | "discord" | "instagram" | "notion" | "github";
+  logo: string;
+  title: string;
+  description: string;
+  buttonText: string;
+  href: string;
+}
+
+export const links_community: LinkPage[] = [
+  {
+    type: "whatsapp",
+    logo: "/Svg/whatsapp.svg",
+    title: "Comunidad",
+    description: "Nuestra comunidad de WhatsApp para actualizaciones rápidas, anuncios importantes y comunicación informal entre miembros de la comunidad.",
+    buttonText: "Unirse al grupo de WhatsApp",
+    href: "https://chat.whatsapp.com/GZYwzwIY8FZJH8qF1ooUpj"
+  },
+  {
+    type: "discord",
+    logo: "/Svg/discord.svg",
+    title: "Comunidad",
+    description: "Nuestro servidor principal de comunicación. Únete para discusiones en tiempo real, eventos, colaboración en proyectos y soporte entre pares.",
+    buttonText: "Unirse al Discord",
+    href: "https://discord.gg/b58Rhb63W6"
+  }, 
+];
+
+export const social_networks: LinkPage[] = [
+  {
+    type: "instagram",
+    logo: "/Svg/instagram.svg", // Reemplaza con la ruta real del logo si la tienes
+    title: "DATASAM",
+    description: "Nuestro medio audiovisual principal. Sigue nuestras actualizaciones, eventos y contenido exclusivo de la comunidad.",
+    buttonText: "Seguir",
+    href: "https://www.instagram.com/datasamok/" // Reemplaza con el enlace real
+  },
+  {
+    type: "instagram",
+    logo: "/Svg/instagram.svg", // Reemplaza con la ruta real del logo si la tienes
+    title: "LCD",
+    description: "Instagram oficial de la Licenciatura en Ciencia de Datos. Información académica y eventos relacionados con la carrera.",
+    buttonText: "Seguir",
+    href: "https://www.instagram.com/lcd_unsam/" // Reemplaza con el enlace real
+  },
+  {
+    type: "instagram",
+    logo: "/Svg/instagram.svg", // Reemplaza con la ruta real del logo si la tienes
+    title: "UNSAM",
+    description: "Instagram oficial de la Universidad Nacional de San Martín. Noticias y eventos de toda la universidad.",
+    buttonText: "Seguir",
+    href: "https://www.instagram.com/unsamoficial/" // Reemplaza con el enlace real
+  },
+  {
+    type: "instagram",
+    logo: "/Svg/instagram.svg", // Reemplaza con la ruta real del logo si la tienes
+    title: "ECyT",
+    description: "Instagram de la Escuela de Ciencia y Tecnología. Información específica de nuestra facultad.",
+    buttonText: "Seguir",
+    href: "https://instagram.com/ecyt.unsam" // Reemplaza con el enlace real
+  } 
+];
+
+export const links_data: LinkPage[] = [
+  {
+    type: "notion",
+    logo: "/Svg/notion.svg", // Reemplaza con la ruta real del logo si la tienes
+    title: "Notion",
+    description: "Nuestro repositorio principal de apuntes, guías de estudio, y recursos compartidos por la comunidad. Una fuente invaluable de conocimiento colaborativo.",
+    buttonText: "Acceder a Notion",
+    href: "Enlace a Notion" // Reemplaza con el enlace real
+  },
+  {
+    type: "github",
+    logo: "/Svg/github.svg", // Reemplaza con la ruta real del logo si la tienes
+    title: "GitHub",
+    description: "Explora y contribuye a nuestros proyectos de código abierto. Perfecto para practicar, colaborar y construir tu portafolio.",
+    buttonText: "Ver proyectos en GitHub",
+    href: "Enlace a GitHub" // Reemplaza con el enlace real
+  }
+];
+
+export const codeOfConductLink: LinkPage = {
+  type: "notion",
+  logo: "/Svg/notion.svg",
+  title: "Código de Conducta",
+  description: "Nuestras reglas de convivencia aseguran un ambiente respetuoso y productivo para todos los miembros. Es crucial que todos los miembros lean y sigan estas pautas.",
+  buttonText: "Leer Código de Conducta",
+  href: "Enlace al Código de Conducta" // Reemplaza con el enlace real
+};
