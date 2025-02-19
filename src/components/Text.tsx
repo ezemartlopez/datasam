c3bb9acd0a8f339ce942450e09a5c7d747c58a02
ezemartlopez <zenitram.e14@gmail.com>
2025-02-19 11:31:39 -0300

    componente de texto modificado

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,6 +1,6 @@
 // Component: Text.tsx
 export default function Text({text}: {text: string}) {
   return (
-    <p className="font-rubik text-base font-light text-wrap text-left">{text}</p>
+    <p className="font-rubik text-base font-light text-wrap text-center">{text}</p>
   );
 }
