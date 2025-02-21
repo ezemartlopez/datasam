208e997d72d11405c216da62f1d50ed5fd49462c
ezemartlopez <zenitram.e14@gmail.com>
2025-02-21 15:58:21 -0300

    componente parrafo modificado

--- src/components/Paragraph.tsx
+++ src/components/Paragraph.tsx
@@ -1,6 +1,8 @@
+import classNames from "classnames"
+
 // Component: Paragraph.tsx
-export default function Paragraph({text}: {text: string}) {
+export default function Paragraph({text, bold}: {text: string, bold?: boolean}) {
   return (
-    <p className="font-rubik text-lg sm:text-xl font-light text-wrap text-left">{text}</p>
+    <p className={classNames("font-rubik text-lg sm:text-xl font-light text-wrap text-left", {"font-normal": bold})}>{text}</p>
   );
 }
